df8ba52e0051ec245c69f03c688f58a2d5a8a724
Sufiyan Khan <sufiyan.builds.py@gmail.com>
2026-08-15 19:25:34 +0530

    Config.java

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -3,7 +3,7 @@ package config;
 public class Config {
 
     public static final String API_KEY =
-            "AQ.Ab8RN6J5yoz2VcSVRHqOtwqaZKJZkJ3KIGOAbWlhrpbG95D0dA";
+            "YOUR_API_KEY_HERE";
 
     public static final String MODEL =
             "gemini-2.5-flash";
